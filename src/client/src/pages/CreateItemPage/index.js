ee83815a7084205a019e413b7e979c62747f841c
santraez <santraez@gmail.com>
2023-11-13 21:24:34 -0500

    first commit

--- src/client/src/pages/CreateItemPage/index.js
+++
@@ -1,1 +0,0 @@
-export { default as CreateItemPage } from "./CreateItemPage";
--- src/config/server.js
+++ src/config/server.js
@@ -15,7 +15,6 @@ app.set('pkg', pkg);
 app.get('/api', (req, res) => {
   res.json({
     name: app.get('pkg').name,
-    author: app.get('pkg').author,
     description: app.get('pkg').description,
     version: app.get('pkg').version
   });
